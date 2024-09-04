f196ead278e11bb0ab91fee8fdb23f5a82f90c70
MukulDhiman03 <dhimanmukul307@gmail.com>
2024-09-04 23:28:34 +0530

    add question9

---
+++ question8.js
@@ -0,0 +1,24 @@
+// Create a function which will accepts two arrays arr1 and arr2. The function should return true if every value in arr1 has its corresponding value squared in array2. The frequency of values must be same.
+
+function checkArray(arr1, arr2) {
+  // First, ensure both arrays are of the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Sort both arrays to compare corresponding values
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Math.pow(arr1[i], 2) !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const arr1 = [2, 3, 2];
+const arr2 = [9, 4, 4];
+
+console.log(checkArray(arr1, arr2));
---
+++ question9.js
@@ -0,0 +1,23 @@
+// Given two strings. Find if one string can be formed by rearranging the letters of other string.
+/*
+str1 = "listen"
+str2 = "silent"
+*/
+
+function checkString(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  const arr1 = str1.split("").sort();
+  const arr2 = str2.split("").sort();
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+const str1 = "listen";
+const str2 = "silent";
+
+console.log(checkString(str1, str2));
