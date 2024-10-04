c03fc7046af3832f0ce04a707f04f3f2f564cc3a
MukulDhiman03 <dhimanmukul307@gmail.com>
2024-10-04 19:17:53 +0530

    Map Polyfill

--- Polyfills/Map.js
+++ Polyfills/Map.js
@@ -1,16 +1,14 @@
 const arr = [1, 2, 3, 4, 5, 6];
 // If you replace the function in Array.prototype.myMap with an arrow function (()=>{}), it will behave differently because arrow functions do not have their own this context.
-Array.prototype.myMap = function (callback) {
-  let newArr = [];
-  console.log(this);
-  for (let i = 0; i < this.length; i++) {
-    newArr.push(callback(i, this[i]));
+Array.prototype.myMap = function (cb) {
+  let ref = this;
+  let resArr = [];
+  for (let i = 0; i < ref.length; i++) {
+    resArr.push(cb(ref[i], i, ref));
   }
-  return newArr;
+  return resArr;
 };
-
-const newArr = arr.myMap((index, val) => {
-  return val * index;
+let newArr = arr.myMap((item, index, arr) => {
+  return item * index;
 });
-
 console.log(newArr);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,2 @@
- deposit: function (amount) {
-      balance += amount;
-      console.log(`Deposited: ${amount}, New Balance: ${balance}`);
-    },
+
+});
